4874f79865ac4a29cf3acac1b16cbabb5146cf49
fwallis <f.s.a.wallis-3@prinsesmaximacentrum.nl>
2023-09-11 16:40:06 +0200

    Changed parameter metaData to metaDataRef

--- R/obtainTrainData.R
+++ R/obtainTrainData.R
@@ -2,7 +2,7 @@
 #'
 #' This function is used to generate different training subsets for the majority voting system within the M&M setup.
 #'
-#' @param metaData Metadata file containing the links between the patients and the tumor (sub)type diagnosis.
+#' @param metaDataRef Metadata file containing the links between the patients and the tumor (sub)type diagnosis for the reference cohort.
 #' @param classColumn Column in the metadata file that contains the tumor (sub)type labels.
 #' @param maxSamplesPerType How many samples should we maximally use per tumor (sub)type?
 #' @param nModels How many models should be created for the majority voting system?
@@ -11,26 +11,26 @@
 #' all with the specified maximum samples per tumor (sub)type.
 #' @export
 #'
-obtainTrainData <- function(metaData, classColumn, maxSamplesPerType = 50, nModels = 100) {
+obtainTrainData <- function(metaDataRef, classColumn, maxSamplesPerType = 50, nModels = 100) {
 
   samplesTrainDefList <- list()
-  metaData[, classColumn] <- as.factor(metaData[, classColumn])
+  metaDataRef[, classColumn] <- as.factor(metaDataRef[, classColumn])
 
-  typesInFold <- table(metaData[, classColumn])
+  typesInFold <- table(metaDataRef[, classColumn])
   typeProbs <- 1/sqrt((typesInFold))
 
   for ( j in c(1:nModels)){
     set.seed(j)
     samplesTrainDef <- c()
 
-    samplesTrain <- unique(sample(rownames(metaData),
-                                  prob=typeProbs[metaData[ , classColumn]],
+    samplesTrain <- unique(sample(rownames(metaDataRef),
+                                  prob=typeProbs[metaDataRef[ , classColumn]],
                                   replace = T))
 
-    includedTypes <- unique(metaData[samplesTrain, classColumn])
+    includedTypes <- unique(metaDataRef[samplesTrain, classColumn])
 
     # Look which tumor types are not present in your selected dataset
-    missingType <- metaData[!(metaData[ , classColumn] %in% includedTypes), ]
+    missingType <- metaDataRef[!(metaDataRef[ , classColumn] %in% includedTypes), ]
 
     # If there are tumor types not present, we loop through them all to add
     # one entry for each tumor type to the training samples.
@@ -44,11 +44,11 @@ obtainTrainData <- function(metaData, classColumn, maxSamplesPerType = 50, nMode
     }
 
     #
-    nSamplesPerType <- table(metaData[samplesTrain,classColumn])
+    nSamplesPerType <- table(metaDataRef[samplesTrain,classColumn])
 
 
     for (n in c(1:length(nSamplesPerType))){
-      curSamples <- samplesTrain[metaData[samplesTrain,classColumn] ==
+      curSamples <- samplesTrain[metaDataRef[samplesTrain,classColumn] ==
                                    names(nSamplesPerType)[n]]
 
       # Entries that are present more often than the specified maxSamplesPerType are downsampled to maxSamplesPerType
