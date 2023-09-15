ba132eb02e57a32da3682edec652428b2d87599c
fwallis <f.s.a.wallis-3@prinsesmaximacentrum.nl>
2023-09-15 11:04:38 +0200

    Changed parameter names because they didn't match.
    Also added description.

--- R/predictRiboCounts.R
+++ R/predictRiboCounts.R
@@ -1,23 +1,29 @@
-#' Predict the
+#' Correct new count data for ribodepletion efficacy
 #'
-#' @param riboModel
-#' @param data
+#' This function is designed to use the same ribodepletion correcting model
+#' on the new samples as was used on the reference cohort.
 #'
-#' @return
-#' @export
+#' @param riboModel Model for ribocorrection, normally stored inside the R-object
+#' obtained form running the function createScalingsMajority.
+#' @param data Count data to be transformed.
+#'
+#' @return Count data corrected for the efficacy of the ribodepletion protocol.
+#'
 #'
 predictRiboCounts <- function(riboModel, data) {
   # Predict how much protein coding reads v.s. the ribosomal reads are present within the data
-  relevantCoefficients <- riboModelCounts$relevantCoefficients
-  highMeanGenes <- names(riboModelCounts$meanGenes)
-  meanGenes <- riboModelCounts$meanGenes
-  varGenes <- riboModelCounts$varGenes
-  normalizedData <- apply(data,2,function(x) (x[highMeanGenes]-meanGenes[highMeanGenes])/varGenes[highMeanGenes])
+  relevantCoefficients <- riboModel$relevantCoefficients
+  highMeanGenes <- names(riboModel$meanGenes)
+  meanGenes <- riboModel$meanGenes
+  varGenes <- riboModel$varGenes
+  dataSub <- data[highMeanGenes, ]
+  normalizedData <- apply(dataSub,2,function(x) (x -meanGenes[highMeanGenes])/varGenes[highMeanGenes])
 
   predictProteinCoding <- 1-(relevantCoefficients[1] + t(normalizedData[names(relevantCoefficients)[-1],]) %*% relevantCoefficients[-1])
 
   # Divide counts for each sample by the percentage of protein coding sequence.
   # The less protein coding reads, the higher the scale-up of eventual reads.
+
   data <- sapply(c(1:ncol(data)),function(x) smart.round(data[,x]/predictProteinCoding[x],digits =3))
   colnames(data) <- colnames(normalizedData)
 
