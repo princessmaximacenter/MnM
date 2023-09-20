7b4c1f4db69b4a3188cd9e4dccf5daacba54e71d
fwallis <f.s.a.wallis-3@prinsesmaximacentrum.nl>
2023-09-20 16:23:02 +0200

    Added descriptions to the function getPrincipalComponents.

--- R/getPrincipalComponents.R
+++ R/getPrincipalComponents.R
@@ -1,14 +1,18 @@
-#' Title
+#' Get PCA transformation Majority Classifier
 #'
-#' @param dataTrain
-#' @param samplesTrainDefList
-#' @param classColumn
-#' @param nModels
-#' @param nFeatures
-#' @param nComps
+#' Function to find the optimal principal component analysis (PCA) transformation of the reference cohort
+#' so that a maximal variance within the data is explained. With the parameters, the number of variable features
+#' and the eventual total number of principal components can be adjusted.
 #'
-#' @return
-#' @export
+#' @param dataTrain Data to be used for training, containing the whole reference cohort.
+#' @param samplesTrainDefList List of different training data subsets used for the majority voting system.
+#' @param classColumn Column in the metadata file that contains the tumor (sub)type labels.
+#' @param nModels How many models should be created for the majority voting system?
+#' @param nFeatures How many of the most variable genes within the dataset should we select for principal component analysis (PCA)?
+#' @param nComps How many principal components will be selected after PCA?
+#'
+#' @return List containing both the derived PCA-transformation information ($prList)
+#' and the information needed to scale new sample input data to center the features around 0 ($scaleFeaturesList).
 #'
 getPrincipalComponents <- function(dataTrain,
          samplesTrainDefList,
