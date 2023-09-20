297d5569288982c9e9e851af2abe497214acfb1a
fwallis <f.s.a.wallis-3@prinsesmaximacentrum.nl>
2023-09-20 16:26:24 +0200

    Uploaded function newPredictionsMinority from minorityClassifier.R

---
+++ R/newPredictionsMinority.R
@@ -0,0 +1,63 @@
+newPredictionsMinority <- function(riboModelList, createdModelsMinority, countDataNew,
+                                   outputDir) {
+  # Find the predictions for the test data
+  # PREPARE DATA
+  countDataNew <- apply(countDataNew,2,function(x) (x/sum(x))*1E6)
+
+  countDataNew <- predictRiboCounts(riboModel = riboModelList$riboModel, data = countDataNew)
+
+  dataLogNew <- log(countDataNew + 1) %>% t() %>% as.data.frame()
+  dataLogNew <- dataLogNew[ , createdModelsMinority$reducedFeatures, drop = F]
+
+  # Also create test data and specify which biomaterial IDs are in there
+  #testSamples <- rownames(dataLogNew)
+
+  result <- predictTest(createdModelsMinority$modelList, dataLogNew)
+
+  print("Finished with predicting results")
+
+
+  if (nrow(result) == 1) {
+    randomVector <- paste0("fake", 1:ncol(result)) %>% as.data.frame() %>% t() %>% as.data.frame()
+    colnames(randomVector) <- colnames(result)
+    result1 <- rbind(result, randomVector)
+    probability <-  apply(result1, 1, table)
+  } else {
+    # Find out how often a certain tumor type prediction is made for a specific sample
+    probability <- apply(result, 1, table)
+  }
+
+  # Locate the position of the highest probability
+  positions <- lapply(probability, which.max)
+  positions <- unlist(positions)
+
+  bestFit <- data.frame(predict = rep(NA, times = length(result$fold1)))
+  probabilityScores <- vector()
+
+  # Extract the different calls being made for each sample
+  mostAppearingNames <- lapply(probability, names)
+
+  # Store the one with the highest probability score into the bestFit dataframe
+  for (j in seq(1:length(mostAppearingNames))) {
+    numberPositions <- as.numeric(positions[j])
+    probabilityScores[j] <- probability[[j]][numberPositions]
+    bestFit[j,] <- mostAppearingNames[[j]][numberPositions]
+  }
+
+
+  # Store the bestFit, the Originalcall and the accompanying probability score within the final dataframe.
+  classifications <- cbind(bestFit,
+                           probability = probabilityScores)
+
+  if (nrow(result) == 1) {
+    classifications <- classifications[1, , drop = F]
+
+  }
+  # Make sure that the classifications still have their accompanying biomaterial_id
+  rownames(classifications) <- rownames(result)
+  classificationList <- list(classifications = classifications,
+                             probability = probability)
+  filename <- paste0(outputDir,format(as.Date(Sys.Date(), "%Y-%m-%d"), "%m_%d_%Y"), "/minorityClassifierResult.rds")
+  write_rds(classificationList, file = filename)
+  return(classificationList)
+}
