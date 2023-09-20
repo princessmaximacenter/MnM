e0d3a71edcf6d585a04fa2746760dd2a61ff0f6e
fwallis <f.s.a.wallis-3@prinsesmaximacentrum.nl>
2023-09-20 16:22:42 +0200

    Added an option to substitute names, but I would recommend to maybe remove it again.

--- R/integrateMM.R
+++ R/integrateMM.R
@@ -4,7 +4,8 @@
 #'
 #' @param minority R-object that contains the results from the Minority classifier
 #' @param majority R-object that contains the results from the Majority classifier
-#' @param metaDataRef Metadata-file for the reference cohort.
+#' @param metaDataRef Metadata file containing the links between the patients and
+#' the tumor (sub)type diagnosis within the reference cohort.
 #' @param nModels How many models were used to obtain a final prediction?
 #' @param subtype Do you want to obtain the predictions on the tumor subtype classification level?
 #' If so, use _subtype = T_. If you want to obtain tumor type predictions instead, use _subtype = F_.
@@ -12,7 +13,8 @@
 #' @param higherClassColumn Column name within metadata-file that contains the cancer type labels.
 #' @param crossValidation Specify whether the results are from the cross-validation setup or not.
 #' This is important, as for the cross-validation setup there is a ground truth ($originalCall), while for new predictions there is not.
-#'
+#' @param substituteNames Do you want to substitute some names?
+#' @param substituteBy What do you want to substitute it by?
 #' @return Dataframe showing the top 3 predictions for the tumor (sub)type, together with their probability scores.
 #' @export
 #'
@@ -23,7 +25,13 @@ integrateMM <- function(minority,
                         subtype,
                         classColumn,
                         higherClassColumn,
-                        crossValidation = T) {
+                        crossValidation = T,
+                        changeNames = F,
+                        substituteNames,
+                        substituteBy
+
+
+                        ) {
 
   probabilitiesMinority <- obtainProbabilities(minority,
                                                crossValidation = crossValidation,
@@ -33,9 +41,34 @@ integrateMM <- function(minority,
                                                crossValidation = crossValidation,
                                                nModels = nModels)
 
+  if (changeNames == T) {
+    for (i in seq(1:length(probabilitiesMinority))) {
+      for (j in seq(1:length(substituteNames))) {
+        names(probabilitiesMinority[[i]]) <- gsub(substituteNames[j], substituteBy, names(probabilitiesMinority[[i]]))
+
+      }
+      probabilitiesMinority[[i]] <- tapply(probabilitiesMinority[[i]], names(probabilitiesMinority[[i]]), sum)
+    }
+
+    for (i in seq(1:length(probabilitiesMajority))) {
+      for (j in seq(1:length(substituteNames))) {
+        names(probabilitiesMajority[[i]]) <- gsub(substituteNames[j], substituteBy, names(probabilitiesMajority[[i]]))
+
+      }
+      probabilitiesMajority[[i]] <- tapply(probabilitiesMajority[[i]], names(probabilitiesMajority[[i]]), sum)
+    }
+
+  }
 
   if (subtype == F) {
     linkClassAndHigherClass <- metaDataRef[ , c(classColumn, higherClassColumn)] %>% unique
+    if (changeNames == T) {
+      for (j in seq(1:length(substituteNames))) {
+        linkClassAndHigherClass[,classColumn] <- gsub(substituteNames[j], substituteBy, linkClassAndHigherClass[,classColumn])
+
+      }
+      linkClassAndHigherClass %<>% unique()
+    }
     probabilitiesMinority <- changeSubtypeNameToType(probabilitiesMinority,
                                                           linkClassAndHigherClass = linkClassAndHigherClass,
                                                      classColumn = classColumn,
