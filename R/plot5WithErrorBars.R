d2782492ce663113bee1a8e5bdd96a1b328839ca
fwallis <f.s.a.wallis-3@prinsesmaximacentrum.nl>
2024-02-27 14:38:34 +0100

    Included documentation and altered return-statement, giving either a plot or a dataframe that can be used to create the plot

--- R/plot5WithErrorBars.R
+++ R/plot5WithErrorBars.R
@@ -1,3 +1,33 @@
+#' Plot comparison between classifiers on total results
+#'
+#' With this function, the overall results will be displayed for the comparison between different classifiers.
+#' Error bars will be generated based on different cross-validation runs.
+#'
+#' @param classColumn Column in the metadata file that contains the tumor subtype labels.
+#' @param higherClassColumn Column in the metadata file that contains the tumor type labels.
+#' @param minorityDir Directory in which the minority model(s) are stored for the cross-validation setup.
+#' @param majorityDir Directory in which the majority model(s) are stored for the cross-validation setup.
+#' @param minorityDirTest Directory in which the minority model(s) are store for the test setup.
+#' @param majorityDirTest Directory in which the majority model(s) are store for the test setup.
+#' @param metaDataRef  Metadata file containing the links between the patients and the tumor (sub)type diagnosis within the reference cohort.
+#' @param metaDataTest  Metadata file containing the links between the patients and the tumor (sub)type diagnosis within the test set.
+#' @param otherDataSets List containing a description of the other datasets, with their actual data ($data),
+#'  whether we're looking at the tumor type of subtype ($subtype),
+#'  what is the probability score threshold that should be used for the M&M classifications ($probabilityScoreThreshold),
+#'  whether we're looking at cross-validation or test set results ($TrainOrTest, should be "Train" or "Test"),
+#'  and what is the name that you would like to attach to the subset of the data ($subsetName).
+#' @param throwOut Are there samples you would like to remove from the test set due to poor data quality? If so, add their rownames here.
+#' @param nModels  How many models were created for the majority voting system?
+#' @param nSeedsTrain How many seeds was the cross-validation setup run with?
+#' @param plotResults Do you want to obtain the data or (FALSE) or get the resulting plot (TRUE)?
+#'
+#' @return If plotResults == F, a dataframe containing the accuracy ($meanAccuracy), precision ($meanPrecision),
+#' recall ($meanRecall), and their standard deviations for M&M and other classifirs on a subset of the data ($Subset)
+#' with a specific number of samples ($numberSamples), either within the training data or test set ($TrainOrTest) will be returned.
+#' If plotResults == T, a plot showing the accuracy, precision and recall for M&M and other classifiers for specific subsets of the dataset will be returned.
+#' Included are error bars for the results of the 10 seeds used for the cross-valiation results.
+#'
+#'
 plot5WithErrorBars <- function(classColumn,
                                higherClassColumn,
                                minorityDir,
@@ -9,9 +39,9 @@ plot5WithErrorBars <- function(classColumn,
                                otherDataSets,
                             #   otherDataSetsTest,
                                throwOut = NA,
-                               crossValidation = T,
                                nModels,
-                               nSeeds
+                            nSeedsTrain,
+                            plotResults = F
 ) {
 
   for (i in seq(1:length(otherDataSets))) {
@@ -22,31 +52,34 @@ plot5WithErrorBars <- function(classColumn,
     subsetName <- otherDataSets[[i]]$subsetName
     trainOrTest <- otherDataSets[[i]]$TrainOrTest
     if (trainOrTest == "Train") {
+      nSeeds <- nSeedsTrain
       crossValidation <- T
+      minorityDirectory <- minorityDir
+      majorityDirectory <- majorityDir
     } else {
+      #nSeeds <- 1
       crossValidation <- F
+      minorityDirectory <- minorityDirTest
+      majorityDirectory <- majorityDirTest
     }
     #subset <- rownames(otherDataSet)
 
-  meanAndSDPlotTrain <- calculateMeanAndSDOverall(classColumn = classColumn,
-                                                  higherClassColumn = higherClassColumn,
-                                                  minorityDir = minorityDir,
-                                                  majorityDir = majorityDir,
-                                                  minorityDirTest = minorityDirTest,
-                                                  majorityDirTest = majorityDirTest,
-                                                  metaDataRef = metaDataRef,
-                                                  metaDataTest = metaDataTest,
+  meanAndSDPlotTrain <- calculateMeanAndSDOverall(classColumn,
+                                                  higherClassColumn,
+                                                  minorityDir = minorityDirectory,
+                                                  majorityDir = majorityDirectory,
+                                                  metaDataRef,
+                                                  metaDataTest,
                                                   throwOut = throwOut,
                                                   subtype = subtype,
                                                   crossValidation = crossValidation,
                                                   nModels = nModels,
-                                                  nSeeds = nSeeds,
+                                                  nSeeds  = nSeeds,
                                                   subset = rownames(otherDataSet),
-                                                  probabilityScoreThreshold = probabilityScoreThreshold
-
+                                                  probabilityThreshold = probabilityScoreThreshold
                                                   )
 
-
+  #meanAndSDPlotTrain %<>% filter(nCases == "All")
   meanAndSDPlotTrain$type <- "M&M"
   meanAndSDPlotTrain$Subset <- subsetName
   meanAndSDPlotTrain$TrainOrTest <- trainOrTest
@@ -96,5 +129,10 @@ plot5WithErrorBars <- function(classColumn,
 
  }
   }
-return(meanAndSDPlotTotal)
+  if (plotResults == T) {
+    return(plotComparisonBetweenClassifiers(meanAndSDPlotTotal))
+
+  } else {
+    return(meanAndSDPlotTotal)
+  }
 }
