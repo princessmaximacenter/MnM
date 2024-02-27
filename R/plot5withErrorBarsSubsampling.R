466b2d30f1635b1382c4ec4e5ac082077ec52123
fwallis <f.s.a.wallis-3@prinsesmaximacentrum.nl>
2024-02-27 14:06:39 +0100

    Added documentation and variable plotResults, to link the functions to each other and to allow for immediate plotting of the results

--- R/plot5withErrorBarsSubsampling.R
+++ R/plot5withErrorBarsSubsampling.R
@@ -1,3 +1,32 @@
+#' Plot comparison between classifiers on subsampled results
+#'
+#' With this function, the results of will be displayed for the comparison between different classifiers.
+#' Error bars will be generated based on results for 100 different data subsamples.
+#'
+#' @param classColumn Column in the metadata file that contains the tumor subtype labels.
+#' @param higherClassColumn Column in the metadata file that contains the tumor type labels.
+#' @param minorityDir Directory in which the minority model(s) are stored for the cross-validation setup.
+#' @param majorityDir Directory in which the majority model(s) are stored for the cross-validation setup.
+#' @param minorityDirTest Directory in which the minority model(s) are store for the test setup.
+#' @param majorityDirTest Directory in which the majority model(s) are store for the test setup.
+#' @param metaDataRef  Metadata file containing the links between the patients and the tumor (sub)type diagnosis within the reference cohort.
+#' @param metaDataTest  Metadata file containing the links between the patients and the tumor (sub)type diagnosis within the test set.
+#' @param otherDataSets List containing a description of the other datasets, with their actual data ($data),
+#'  whether we're looking at the tumor type of subtype ($subtype),
+#'  what is the probability score threshold that should be used for the M&M classifications ($probabilityScoreThreshold),
+#'  whether we're looking at cross-validation or test set results ($TrainOrTest, should be "Train" or "Test"),
+#'  and what is the name that you would like to attach to the subset of the data ($subsetName).
+#' @param subsamplePercentage Which percentage of samples would you want to subsample each time?
+#' @param nModels  How many models were created for the majority voting system?
+#' @param nSeeds How many seeds was the cross-validation setup run with?
+#' @param plotResults Do you want to obtain the data or (FALSE) or get the resulting plot (TRUE)?
+#'
+#' @return  If plotResults == F, a dataframe containing the accuracy ($meanAccuracy), precision ($meanPrecision),
+#' recall ($meanRecall), and their standard deviations for M&M and other classifirs on a subset of the data ($Subset)
+#' with a specific number of samples ($numberSamples), either within the training data or test set ($TrainOrTest) will be returned.
+#' If plotResults == T, a plot showing the accuracy, precision and recall for M&M and other classifiers for specific subsets of the dataset will be returned.
+#' Included are error bars for the results of the 10 seeds used for the cross-valiation results.
+#'
 plot5withErrorBarsSubsampling <- function(classColumn,
                                           higherClassColumn,
                                           minorityDir,
@@ -9,10 +38,9 @@ plot5withErrorBarsSubsampling <- function(classColumn,
                                           otherDataSets,
                                           subsamplePercentage,
                                           #   otherDataSetsTest,
-                                          throwOut = NA,
-                                          crossValidation = T,
                                           nModels,
-                                          nSeeds
+                                          nSeeds,
+                                          plotResults = F
 ) {
 
   predictionsMMAverageList <- combineSeedPredictions(nSeeds = nSeeds,
@@ -21,7 +49,8 @@ plot5withErrorBarsSubsampling <- function(classColumn,
                                                      subtype = F,
                                                      classColumn = classColumn,
                                                      higherClassColumn = higherClassColumn,
-                                                     metaDataRef = metaDataRef
+                                                     metaDataRef = metaDataRef,
+                                                     nModels = nModels
   )
   predictionsMMAverage <- predictionsMMAverageList$predictionsMMFinal
 
@@ -31,7 +60,8 @@ plot5withErrorBarsSubsampling <- function(classColumn,
                                                             subtype = T,
                                                             classColumn = classColumn,
                                                             higherClassColumn = higherClassColumn,
-                                                            metaDataRef = metaDataRef
+                                                            metaDataRef = metaDataRef,
+                                                            nModels = nModels
   )
 
   predictionsMMAverageSubtype <- predictionsMMAverageSubtypeList$predictionsMMFinal
@@ -48,7 +78,8 @@ plot5withErrorBarsSubsampling <- function(classColumn,
                                               higherClassColumn = higherClassColumn,
                                               nModels = nModels,
                                               subtype = T,
-                                              crossValidation = F)
+                                              crossValidation = F
+                                              )
 
   predictionsMMSubtypeTest <- predictionsMMSubtypeTestList$predictionsMMFinal
 
@@ -179,6 +210,11 @@ plot5withErrorBarsSubsampling <- function(classColumn,
                                tTestDFTotal)
     }
   }
-return(tTestDFCombined)
+  if (plotResults == T) {
+    return(plotComparisonBetweenClassifiers(tTestDFCombined, subsampling = T))
+
+  } else {
+    return(tTestDFCombined)
+  }
 
 }
