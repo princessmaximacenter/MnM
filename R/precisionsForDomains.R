6f917da03b89d74b77a273ab08cba7c6ebf9f34d
fwallis <f.s.a.wallis-3@prinsesmaximacentrum.nl>
2024-04-18 13:49:15 +0200

    Documentation

--- R/precisionsForDomains.R
+++ R/precisionsForDomains.R
@@ -1,16 +1,14 @@
 #' Compare results between samples from different domains
 #'
-#' @param nSeeds How many seeds was the cross-validation setup run with?
-#' @param classColumn Column in the metadata file that contains the tumor subtype labels.
-#' @param higherClassColumn Column in the metadata file that contains the tumor type labels.
-#' @param domainColumn Column in the metadata file that contains the domain labels.
-#' @param crossValidation Specify whether the results are from the cross-validation setup or not.
+#' @param classColumn Column in the original metadata file that contains the tumor subtype labels.
+#' @param higherClassColumn Column in the original metadata file that contains the tumor type labels.
+#' @param domainColumn Column in the original metadata file that contains the domain labels.
 #' @param minorityDir Directory in which the minority model(s) are stored.
 #' @param majorityDir Directory in which the majority model(s) are stored.
-#' @param nModels How many models were created for the majority voting system?
 #' @param subtype Do you want to obtain the predictions on the tumor subtype classification level?
-#' @param throwOut Are there samples you would like to remove from the test set due to poor data quality? If so, add their rownames here.
 #' @param metaDataTest Metadata file containing the links between the patients and the tumor (sub)type diagnosis within the test set.
+#' This file needs to have the same column names for the tumor subtype labels (classColumn),
+#' tumor type labels (higherClassColumn) and domain labels (domainColumn).
 #' @param probabilityThreshold What is the probability score threshold you would like to use to call a classification 'confident' for M&M?
 #' @param metaDataRef Metadata file containing the links between the patients and the tumor (sub)type diagnosis.
 #'
@@ -20,33 +18,55 @@
 #' $sdPrecision, $sdRecall), and the total number of samples associated to the domain group ($nSamples)
 #' @export
 #'
-precisionsForDomains <- function( nSeeds,
+precisionsForDomains <- function(
           classColumn,
           higherClassColumn,
           domainColumn,
-          crossValidation,
           minorityDir,
           majorityDir,
-          nModels,
           subtype,
-          throwOut,
           metaDataTest,
           probabilityThreshold,
           metaDataRef) {
 
   `%notin%` <- Negate(`%in%`)
-  for (i in seq(1:nSeeds)) {
-    if (crossValidation == T) {
-      minorityDoc <- paste0(minorityDir, "seed",i, "/crossValidationMinorityResults.rds")
-      majorityDoc <- paste0(majorityDir, "seed",i, "/crossValidationMajorityResults.rds")
-    } else {
-      minorityDoc <- minorityDir
-      majorityDoc <- majorityDir
+  if ("minorityClassifierResult.rds" %notin% list.files(minorityDir)) {
+    crossValidation <- T
+    allDirsMinority <- list.dirs(minorityDir, recursive = F)
+    allDirsMajority <- list.dirs(majorityDir, recursive = F)
+    selectedDirsMinority <- allDirsMinority[grep("seed", allDirsMinority)]
+    selectedDirsMajority <- allDirsMajority[grep("seed", allDirsMajority)]
 
+    print(paste0("Found ",length(selectedDirsMajority), " directories with different cross-validation runs.",
+                 " Calculating average performance values for all combined."))
+    if (length(selectedDirsMinority) != length(selectedDirsMajority)) {
+      stop("The number of models for the minority and majority classifier are not the same.
+         Please check your models within the minorityDir and majorityDir that the
+         same seeds have been used for the generation of a minority and a majority classifier.")
+    } else if (!all.equal(selectedDirsMajority, selectedDirsMinority) ) {
+      stop("Please make sure you run the crossvalidation with the same seed for complementary classifications,
+         and store them in the same directory.")
+    }
+  } else {
+    selectedDirsMajority <- majorityDir
+    crossValidation <- F
+  }
+
+  for (i in seq(1:length(selectedDirsMajority))) {
+    if (crossValidation == T) {
+      minorityDoc <- paste0(selectedDirsMinority[i],"/crossValidationMinorityResults.rds")
+      majorityDoc <- paste0(selectedDirsMajority[i],"/crossValidationMajorityResults.rds")
+
+    } else {
+      minorityDoc <- paste0(minorityDir, "/minorityClassifierResult.rds")
+      majorityDoc <- paste0(majorityDir, "/majorityClassifierResult.rds")
+      metaData <- metaDataTest
+      print("No original call found, adding it from metaDataTest")
     }
     minority <- readRDS(minorityDoc)
     majority <- readRDS(majorityDoc)
 
+    metaData <- minority$metaDataRef
     predictionsMMFinalList <- integrateMM(minority = minority,
                                           majority = majority,
                                           subtype = subtype,
@@ -55,23 +75,17 @@ precisionsForDomains <- function( nSeeds,
     )
 
     predictionsMMFinal <- predictionsMMFinalList$predictionsMMFinal
-    if (crossValidation == F ) {
-      predictionsMMFinal %<>% filter(rownames(.) %notin% throwOut)
-      metaDataTest %<>% filter(rownames(.) %notin% throwOut)
-      if (subtype == F) {
-        predictionsMMFinal$originalCall <- metaDataTest[rownames(predictionsMMFinal), higherClassColumn]
+    if ("originalCall" %in% colnames(predictionsMMFinal) ) {
+      metaData <- minority$metaDataRef
+      } else if (subtype == F) {
+        predictionsMMFinal$originalCall <- metaData[rownames(predictionsMMFinal), higherClassColumn]
       } else {
-        predictionsMMFinal$originalCall <- metaDataTest[rownames(predictionsMMFinal), classColumn]
+        predictionsMMFinal$originalCall <- metaData[rownames(predictionsMMFinal), classColumn]
       }
-    }
 
-    ourDomains <- unique(metaDataRef[,domainColumn])
 
-    if (crossValidation == F) {
-      metaData <- metaDataTest
-    } else {
-      metaData <- metaDataRef
-    }
+    ourDomains <- unique(minority$metaDataRef[,domainColumn])
+
 
     for (j in seq(1:length(ourDomains))) {
 
