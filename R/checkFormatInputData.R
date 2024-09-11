4ea361ae5ead7e966c7b73a4ccd0e5b654af467a
fwallis <f.s.a.wallis-3@prinsesmaximacentrum.nl>
2024-09-11 14:37:32 +0200

    Combined all the checking statements for the input data or test data into one file / function.

---
+++ R/checkFormatInputData.R
@@ -0,0 +1,84 @@
+checkFormatInputData <- function(sampleColumn,
+                                 classColumn,
+                                 higherClassColumn,
+                                 domainColumn,
+                                 metaDataRef,
+                                 countDataRef,
+                                 outputDir = NA,
+                                 saveModel) {
+
+  if (sampleColumn %notin% base::colnames(metaDataRef)) {
+    base::stop("The column you specified for the sample IDs is not present within metaDataRef. Please check the sampleColumn.")
+  } else if (classColumn %notin% base::colnames(metaDataRef)) {
+    base::stop("The column you specified for the tumor subtype labels is not present within metaDataRef. Please check the classColumn")
+  } else if (higherClassColumn %notin% base::colnames(metaDataRef)){
+    base::stop("The column you specified for the tumor type labels is not present within metaDataRef. Please check the higherClassColumn")
+  } else if (domainColumn %notin% base::colnames(metaDataRef)) {
+    base::stop("The column you specified for the tumor domain labels is not present within metaDataRef. Please check the domainColumn")
+  }
+
+  # Make sure the metadata and count data are in the right format and same order
+  if (base::nrow(metaDataRef) != base::ncol(countDataRef)) {
+    base::stop("The number of samples do not match between the metadata and the count data. Please make sure you include all same samples in both objects.")
+  } else if (base::all(metaDataRef[, sampleColumn] %notin% base::colnames(countDataRef))) {
+    base::stop("Your input data is not as required. Please make sure your sample IDs are stored in the sampleColumn, and in the column names of the count data")
+  }
+
+
+  if (base::is.numeric(countDataRef) != T) {
+    base::stop("Your input data is not as required. Please make sure your countDataRef object only contains numerical count data and is a matrix.")
+
+  }
+
+  # Include a statement to store the classColumn, higherClassColumn and domainColumn
+  base::print(base::paste0("The column used for tumor subtypes labels within the metadata, used for model training purposes, is: ", classColumn, ', containing values such as: '))
+  base::print(base::unique(metaDataRef[,classColumn])[1:3])
+
+  base::print(base::paste0("The column used for tumor type labels within the metadata, is: ", higherClassColumn,', containing values such as: '))
+  base::print(base::unique(metaDataRef[,higherClassColumn])[1:3])
+
+  base::print(base::paste0("The column used for tumor domain labels within the metadata, is: ", domainColumn, ', containing values such as: '))
+  base::print(base::unique(metaDataRef[,domainColumn])[1:3])
+  base::print("If any of these are incorrect, specify a different 'classColumn' (subtype), 'higherClassColumn' (tumor type) or 'domainColumn' (domain) to function as labels.")
+
+  if (saveModel == T & !base::dir.exists(outputDir)) {
+    checkDirectory <- base::tryCatch(base::dir.create(outputDir))
+    if (checkDirectory == F) {
+      base::stop(base::paste0("The directory you want the classification to be saved in cannot be created due to an error in the directory path.",
+                              " Please check the spelling of your specified outputDir - it is probable the parent-directory does not exist."))
+    }
+  }
+
+}
+
+
+checkFormatTestData <- function(countDataNew,
+                                countDataRef,
+                                outputDir,
+                                saveModel) {
+
+  # Check whether the genes are within the rows of countDataNew
+  geneOverlap <- sum(rownames(countDataNew) %in% rownames(countDataRef))
+
+   if(geneOverlap == 0) {
+   base::stop("Your input data is not as required. Please make sure your genes are in the rownames of countDataNew and are in the format of HGNC gene names.")
+ } else if(geneOverlap < 0.6 * nrow(countDataRef)) {
+  base::cat("Please note that there is less than 60% overlap between the genes within the reference cohort and countDataNew.\n")
+
+ }
+
+  # Check whether counts are supplied within the RNA-seq counts
+  if (base::is.numeric(countDataNew) != T) {
+    base::stop("Your input data is not as required. Please make sure your countDataNew object only contains numerical count data and is a matrix.")
+  }
+
+  # Generate the directory, if not possible abort
+  if (saveModel == T & !base::dir.exists(outputDir)) {
+    checkDirectory <- base::tryCatch(base::dir.create(outputDir))
+    if (checkDirectory == F) {
+      base::stop("The directory you want the classification to be saved in cannot be created due to an error in the directory path. Please check the spelling of your specified outputDir.")
+    }
+  }
+
+}
+
