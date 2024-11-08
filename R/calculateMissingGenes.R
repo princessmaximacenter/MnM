485c78f9f8f0f53f7ab77253c480d5906a55ec69
fwallis <f.s.a.wallis-3@prinsesmaximacentrum.nl>
2024-11-08 16:02:13 +0100

    Bound all functions to package

--- R/calculateMissingGenes.R
+++ R/calculateMissingGenes.R
@@ -6,20 +6,22 @@
 #' Samples are in the columns, different RNA-transcripts in the rows.
 #' @param neededGenes RNA-transcripts that will be used within the classification process and are required for M&M to run.
 #' Therefore, these genes will be imputed in case they are missing from countDataNew.
+#' @param whichK The number of neighbor datapoints that need to be used to calculate missing RNA-transcripts from.
+#' @import impute
 #'
 #' @return Matrix containing the RNA-transcript per million data for the new samples to be classified,
 #' now containing all the needed genes for the classification process.
 calculateMissingGenes <- function(countDataRef,
                                   countDataNew,
                                   neededGenes,
-                                  whichK = 10) {
+                                  whichK = 3) {
 
-  missingGenes <- neededGenes[neededGenes %notin% rownames(countDataNew)]
+  missingGenes <- neededGenes[neededGenes %notin% base::rownames(countDataNew)]
 
-  missingDF <- matrix(NA, nrow = length(missingGenes), ncol = ncol(countDataNew))
-  rownames(missingDF) <- missingGenes
-  colnames(missingDF) <- colnames(countDataNew)
-  newDataFrame <- rbind(countDataNew,
+  missingDF <- matrix(NA, nrow = length(missingGenes), ncol = base::ncol(countDataNew))
+  base::rownames(missingDF) <- missingGenes
+  base::colnames(missingDF) <- base::colnames(countDataNew)
+  newDataFrame <- base::rbind(countDataNew,
                         missingDF)
 
   countDataNewSubset <- newDataFrame[neededGenes,]
@@ -34,25 +36,25 @@ calculateMissingGenes <- function(countDataRef,
 
   # Subdivide dataset in chunks so that there are always more reference samples than samples to impute
 
-  howManySplits <- ceiling(ncol(newDataFrame) / 500)
+  howManySplits <- base::ceiling(base::ncol(newDataFrame) / 500)
 
-  splits <- rep(1:howManySplits, each = 500)
-  splits <- splits[1:ncol(newDataFrame)]
+  splits <- base::rep(1:howManySplits, each = 500)
+  splits <- splits[1:base::ncol(newDataFrame)]
 
   for (i in unique(splits)) {
-    combiSubset <- cbind(countDataRefSubset, countDataNewSubset[,splits == i])
-    imputedDataComplete <- hush(impute::impute.knn(as.matrix(combiSubset), k = whichK))
+    combiSubset <- base::cbind(countDataRefSubset, countDataNewSubset[,splits == i])
+    imputedDataComplete <- hush(impute::impute.knn(base::as.matrix(combiSubset), k = whichK))
 
-    imputedData <- imputedDataComplete$data[missingGenes,colnames(countDataNewSubset[,splits == i])]
+    imputedData <- imputedDataComplete$data[missingGenes,base::colnames(countDataNewSubset[,splits == i])]
 
     if (i == 1) {
       imputedDataTotal <- imputedData
     } else {
-      imputedDataTotal <- cbind(imputedDataTotal, imputedData)
+      imputedDataTotal <- base::cbind(imputedDataTotal, imputedData)
     }
   }
 
-  countDataNewTotal <- rbind(countDataNew,
+  countDataNewTotal <- base::rbind(countDataNew,
                              imputedDataTotal)
   return(countDataNewTotal)
 }
